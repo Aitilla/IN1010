1faf4cbd497128452eab0a0c616764d85b5ad5be
Aitilla <jhertaas@gmail.com>
2024-01-15 11:29:41 +0100

    compiled

--- inTiTiEn.java
+++ inTiTiEn.java
@@ -1,3 +1,12 @@
 public class inTiTiEn {
+    public static void main(String[] args) {
+        // int myNum = 5;
+        // float myFloatNum = 5.99f;
+        // char myLetter = 'D';
+        // boolean myBool = true;
+        // String myText = "Hello";
+        System.out.println("hello world");
+        
+    }
     
 }
