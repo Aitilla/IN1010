4aac7e8dea3a7e12f7c0559b4d351c6752f74c02
Aitilla <jhertaas@gmail.com>
2024-01-19 09:36:21 +0100

    task2

--- bil2.java
+++ bil2.java
@@ -1,5 +1,13 @@
 
 
 public class bil2 {
+    String carNum;
+
+    public bil2(String carNum){
+        this.carNum = carNum;
+    }
     
+    public void call(){
+        System.out.println("I am a car " + carNum);
+    }
 }
--- bilBruk2.java
+++ bilBruk2.java
@@ -1,5 +1,8 @@
 
 
 public class bilBruk2 {
-
+    public static void main(String[] args) {
+        bil2 car = new bil2("EK91902");
+        car.call();
+    }
 }
